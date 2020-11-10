df1b79da77eb14854d810010fe5860cceacd8089
boros7266 <benceboros11@gmail.com>
2020-11-10 01:45:29 +0100

    c_program

--- obj_parser/src/vertex.c
+++ obj_parser/src/vertex.c
@@ -12,8 +12,6 @@ int read_vertex(Vertex* vertex, const char* text)
     }
     if (text[i] != 0) {
         vertex->x = atof(&text[i]);
-	printf("vertexx: %lf",vertex->x);
-	printf("\n");
     }
     else {
         printf("The x value of vertex is missing!\n");
@@ -27,8 +25,6 @@ int read_vertex(Vertex* vertex, const char* text)
     }
     if (text[i] != 0) {
         vertex->y = atof(&text[i]);
-	printf("vertexy: %lf",vertex->y);
-	printf("\n");
     }
     else {
         printf("The y value of vertex is missing!\n");
@@ -42,8 +38,6 @@ int read_vertex(Vertex* vertex, const char* text)
     }
     if (text[i] != 0) {
         vertex->z = atof(&text[i]);
-	printf("vertexz: %lf",vertex->z);
-	printf("\n");
     }
     else {
         printf("The z value of vertex is missing!\n");
